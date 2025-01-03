de2ebcc2ca5f15be0f6e18f4fc83b372f22fcb32
Luan Pham <luanpm88@gmail.com>
2025-01-03 10:40:24 +0700

    breeze

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\SurveyController;
@@ -7,6 +8,18 @@ use App\Http\Controllers\SurveyController;
 Route::get('/', function () {
     return redirect()->action([SurveyController::class, 'index']);
 });
-
+     
 Route::resource('backend/users', UserController::class);
 Route::resource('backend/surveys', SurveyController::class);
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
